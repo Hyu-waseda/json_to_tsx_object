6c21b46e6cf6349a9d10608daabf6cd75addbabb
Hyu-waseda <okumura.hyu@akane.waseda.jp>
2023-11-17 18:27:50 +0900

    update: main.py

--- main.py
+++ main.py
@@ -49,17 +49,50 @@ def translate_text(text, target_language):
     translation = translator.translate(text, dest=target_language)
     return translation.text
 
-def translate_dict(data, target_languages):
+# def translate_dict(data, target_languages):
+#     translated_data = {}
+#     for target_language in target_languages:
+#         translated_data[target_language] = {}
+#         for key, value in data.items():
+#             if isinstance(value, dict):
+#                 translated_data[target_language][key] = translate_dict(value, [target_language])
+#             elif isinstance(value, list):
+#                 # print("value: ")
+#                 # print(value)
+#                 translated_data[target_language][key] = [translate_text(str(item), target_language) for item in value]
+
+#                 # # 新しいリストを作成して翻訳済みのデータを格納
+#                 # translated_values = []
+#                 # for item in value:
+#                 #     # print(str(item))
+#                 #     translated_value = translate_text(str(item), target_language)
+#                 #     # print(translated_value)
+
+#                 #     translated_values.append(translated_value)
+
+#                 # # translated_data 辞書に新しいエントリを追加
+#                 # if target_language not in translated_data:
+#                 #     translated_data[target_language] = {}
+#                 # translated_data[target_language][key] = translated_values
+
+#             else:
+#                 translated_data[target_language][key] = translate_text(str(value), target_language)
+#         time.sleep(10)
+#     return translated_data
+
+def translate_dict(data, target_language):
     translated_data = {}
-    for target_language in target_languages:
-        translated_data[target_language] = {}
-        for key, value in data.items():
-            if isinstance(value, dict):
-                translated_data[target_language][key] = translate_dict(value, [target_language])
-            else:
-                translated_data[target_language][key] = translate_text(str(value), target_language)
-        time.sleep(5)
+    for key, value in data.items():
+        if isinstance(value, dict):
+            translated_data[key] = translate_dict(value, target_language)
+        elif isinstance(value, list):
+            translated_data[key] = [translate_text(str(item), target_language) for item in value]
+        else:
+            translated_data[key] = translate_text(str(value), target_language)
+    time.sleep(10)
     return translated_data
+
+
 
 def create_language_folders(output_folder, target_languages, sub_directory):
     for lang in target_languages:
@@ -73,20 +106,24 @@ def translate_file(input_file, output_folder, target_languages, output_file_name
     create_language_folders(output_folder, target_languages, sub_directory)
 
     for target_language in target_languages:
-        translated_data = translate_dict(data, [target_language])
+        # translated_data = translate_dict(data, [target_language])
+        translated_data = translate_dict(data, target_language)
+
 
         output_file = os.path.join(output_folder, target_language, sub_directory, output_file_name + "_" + target_language + ".tsx")
         with open(output_file, 'w', encoding='utf-8') as file:
             file.write("export const {} = ".format(output_file_name + "_" + target_language.upper()))
-            json.dump(translated_data[target_language], file, ensure_ascii=False, indent=2)
+            # json.dump(translated_data[target_language], file, ensure_ascii=False, indent=2)
+            json.dump(translated_data, file, ensure_ascii=False, indent=2)
+
             file.write("\n\n" + "export default {}".format(output_file_name + "_" + target_language.upper()))
             print("{} is translated.".format(target_language))
 
 if __name__ == "__main__":
-    input_file = "jsons/components/organisms/CookieBanner.json"
-    output_file_name = "CookieBanner"
+    input_file = "jsons/pages/Index.json"
+    output_file_name = "Index"
     output_folder = "translate"
-    sub_directory = "components/organisms"
+    sub_directory = "pages"
 
     target_languages = ["en", "zh-cn", "es", "ar", "fr", "ja"]
 
